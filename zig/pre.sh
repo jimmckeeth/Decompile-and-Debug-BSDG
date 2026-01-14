1bc8e9a6ecf2c096b0cfc8212fc21153389b1da3
Jim McKeeth <jim@mckeeth.org>
2026-01-13 17:35:45 -0700

    prereq

---
+++ zig/pre.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# sudo snap install zig --classic --beta
+
+wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
+tar -xf zig-linux-x86_64-0.13.0.tar.xz
+sudo mv zig-linux-x86_64-0.13.0 /usr/local/bin/zig-dir
+sudo ln -s /usr/local/bin/zig-dir/zig /usr/local/bin/zig
+
+# flatpak install flathub org.ziglang.Zig
